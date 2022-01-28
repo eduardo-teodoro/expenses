f8b785425530bb6495021e8ea72d300f7b67270f
Eduardo Teodoro <eduardocteodoro@gmail.com>
2022-01-27 21:40:19 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/et/expenses/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.et.expenses
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,75 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final void Function(String) onRemove;
+
+  const TransactionList({Key? key, required this.transactions, required this.onRemove})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      //renderizando os itens aos poucos
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  "Nenhuma transação cadastrada!",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(height: 20),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    //ajusta a imagem dentro da área
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            )
+          : ListView.builder(
+              //quantidade de itens disponiveis
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                final tr = transactions[index];
+
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: FittedBox(
+                          child: Text("R\$${tr.value}"),
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat("d MMM y").format(tr.date),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => onRemove(tr.id),
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
